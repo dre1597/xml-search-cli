246adabe13485f09be045fbf18bcd3987b0698fa
André Pereira <andrepereira97@yahoo.com>
2024-01-09 16:50:39 -0300

    :sparkles: add version 1

---
+++ README.md
@@ -0,0 +1,39 @@
+# XML String search CLI
+
+(EN)
+
+Search for a string in an XML file
+
+## Before Usage
+
+Ensure that the `assets/input` folder exists and contains at least one XML file.
+Ensure that the `assets/output` folder exists and is empty.
+
+run `npm install`
+
+## Usage
+
+`npm start <search>`
+
+## Examples
+
+`npm start 12345678900`
+
+(PT)
+
+Pesquisa uma string em um arquivo XML
+
+## Antes de Usar
+
+Verifique se o diretório `assets/input` existe e contém pelo menos um arquivo XML.
+Verifique se o diretório `assets/output` existe e estiver vazio.
+
+run `npm install`
+
+## Uso
+
+`npm start <search>`
+
+## Exemplos
+
+`npm start 12345678900`
---
+++ src/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const { Command } = require('commander');
+const parseString = promisify(require('xml2js').parseString);
+
+const program = new Command();
+
+const inputFolder = path.join(__dirname, '../assets/input');
+const outputFolder = path.join(__dirname, '../assets/output');
+
+program
+  .version('1.0.0')
+  .description('Search for a string in XML files and copy matching files to the output folder')
+  .arguments('<searchString>')
+  .action(async (searchString) => {
+    try {
+      const files = await promisify(fs.readdir)(inputFolder);
+
+      for (const file of files) {
+        if (path.extname(file).toLowerCase() === '.xml') {
+          const filePath = path.join(inputFolder, file);
+          const containsString = await checkForString(filePath, searchString);
+
+          if (containsString) {
+            await copyFile(filePath, outputFolder);
+            console.log(`String found in ${file}. File copied to ${outputFolder}`);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error reading or copying files:', error.message);
+    }
+  });
+
+program.parse(process.argv);
+
+async function checkForString(file, searchString) {
+  try {
+    const fileContent = await promisify(fs.readFile)(file, 'utf-8');
+    return fileContent.includes(searchString);
+  } catch (error) {
+    console.error(`Error reading ${file}:`, error.message);
+    return false;
+  }
+}
+
+async function copyFile(source, destinationFolder) {
+  const fileName = path.basename(source);
+  const destination = path.join(destinationFolder, fileName);
+
+  return promisify(fs.copyFile)(source, destination);
+}
